e7317b45d811d592e5c60f2708fa2b4317398a18
hzsenko <hzsenko@mail.ru>
2017-02-26 18:51:54 +0700

    gulp task for build project & minify img

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,33 @@ var gulp = require('gulp'),
 	cleanCSS = require('gulp-clean-css'),
 	rename = require('gulp-rename'),
 	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	autoprefixer = require('gulp-autoprefixer'),
+	del = require('del'),
+	imagemMin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant'),
+	cache = require('gulp-cache');
+
+//clear
+gulp.task('clear', function(){
+	return del.sync('dist');
+});
+
+//clear cache
+gulp.task('clearCache', function(){
+	return cache.clearAll();
+});
+
+gulp.task('img', function(){
+	return gulp.src('app/img/**/*')
+		.pipe(cache(imagemMin({
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			une: [pngquant()]
+		})))
+		.pipe(gulp.dest('dist/img'));
+});
 
 //less
 gulp.task('less', function(){
@@ -18,6 +44,10 @@ gulp.task('less', function(){
 	styles.forEach(function(item){
 		gulp.src(item)
 		.pipe(less())
+		.pipe(autoprefixer({
+			browsers: ['last 15 versions'],
+			cascade: true
+		}))
 		.pipe(cleanCSS())
 		.pipe(rename({suffix: ".min"}))
 		.pipe(gulp.dest('app/css'));
@@ -45,7 +75,28 @@ gulp.task('js-common', function(){
 		.pipe(gulp.dest('app/js'));
 });
 
+
+gulp.task('build',['clear','img','less','js-libs','js-common'], function(){
+	var lessBuild = gulp.src('app/less/**/*.less')
+		.pipe(gulp.dest('dist/less'));
+
+	var cssBuild = gulp.src('app/css/**/*.css')
+		.pipe(gulp.dest('dist/css'));
+
+	var fontsBuild = gulp.src('app/fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'));
+
+	var jsBuild = gulp.src('app/js/**/*.js')
+		.pipe(gulp.dest('dist/js'));
+
+	var htmlBuild = gulp.src('app/*.html')
+		.pipe(gulp.dest('dist'));
+
+});
+
+//watch
 gulp.task('watch', ['less', 'js-libs','js-common'], function(){
 	gulp.watch('app/less/template/**/*.less', ['less']);
 	gulp.watch('app/js/**/*.js', ['js-common']);
 });
+
